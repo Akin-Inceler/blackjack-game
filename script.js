ad3774d29da50ddd1b68d2b6349f6ef5b4b96a7c
Akin-Inceler <akin2001inceler@gmail.com>
2026-04-11 17:41:04 +0200

    fix: update card initialization to use getRandomCard function for dynamic gameplay

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let firstCard = 10
-let secondCard = 1
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }   
+}
+
+
+
+
 
 function startGame() {
     renderGame()
@@ -40,7 +56,7 @@ function sumCard() {
 }
 function newCard() {
     if (isAlive === true && hasBlackJack === false) {
-        let card = 6
+        let card = getRandomCard()
         sum += card
         cards.push(card)
         renderGame()
